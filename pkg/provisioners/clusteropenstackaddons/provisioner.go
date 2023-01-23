9dee05e697282dec25943eb9bdd86a9a1c7fe14a
Simon Murray <spjmurray@yahoo.co.uk>
2023-01-23 14:43:50 +0000

    Refactor Remote Cluster Handling
    Ostensibly it's just siphoning this off into a provisioner type
    interface, however reality is somewhat different.  This at least
    abstracts things away into algorithms and context/technology specific
    handlers.

--- pkg/provisioners/clusteropenstackaddons/provisioner.go
+++ pkg/provisioners/clusteropenstackaddons/provisioner.go
@@ -18,22 +18,15 @@ package clusteropenstackaddons
 
 import (
 	"context"
-	"fmt"
 
 	unikornv1alpha1 "github.com/eschercloudai/unikorn/pkg/apis/unikorn/v1alpha1"
-	argocdclient "github.com/eschercloudai/unikorn/pkg/argocd/client"
-	argocdcluster "github.com/eschercloudai/unikorn/pkg/argocd/cluster"
 	"github.com/eschercloudai/unikorn/pkg/constants"
 	"github.com/eschercloudai/unikorn/pkg/provisioners"
 	"github.com/eschercloudai/unikorn/pkg/provisioners/cilium"
+	"github.com/eschercloudai/unikorn/pkg/provisioners/clusteropenstack"
 	"github.com/eschercloudai/unikorn/pkg/provisioners/concurrent"
 	"github.com/eschercloudai/unikorn/pkg/provisioners/openstackcloudprovider"
-	"github.com/eschercloudai/unikorn/pkg/provisioners/vcluster"
-	"github.com/eschercloudai/unikorn/pkg/util/retry"
-
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/tools/clientcmd"
-	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	"github.com/eschercloudai/unikorn/pkg/provisioners/remotecluster"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -43,15 +36,19 @@ type Provisioner struct {
 	// client provides access to Kubernetes.
 	client client.Client
 
+	// vclusterClient provides client access inside the control plane.
+	vclusterClient client.Client
+
 	// cluster is the Kubernetes cluster we're provisioning.
 	cluster *unikornv1alpha1.KubernetesCluster
 }
 
 // New returns a new initialized provisioner object.
-func New(ctx context.Context, client client.Client, cluster *unikornv1alpha1.KubernetesCluster) (*Provisioner, error) {
+func New(ctx context.Context, client, vclusterClient client.Client, cluster *unikornv1alpha1.KubernetesCluster) (*Provisioner, error) {
 	provisioner := &Provisioner{
-		client:  client,
-		cluster: cluster,
+		client:         client,
+		vclusterClient: vclusterClient,
+		cluster:        cluster,
 	}
 
 	return provisioner, nil
@@ -61,92 +58,37 @@ func New(ctx context.Context, client client.Client, cluster *unikornv1alpha1.Kub
 var _ provisioners.Provisioner = &Provisioner{}
 
 // newOpenstackCloudProviderProvisioner wraps up OCP provisioner configuration.
-func (p *Provisioner) newOpenstackCloudProviderProvisioner() *openstackcloudprovider.Provisioner {
-	return openstackcloudprovider.New(p.client, p.cluster, p.argoCDClusterName())
+func (p *Provisioner) newOpenstackCloudProviderProvisioner(remote string) *openstackcloudprovider.Provisioner {
+	return openstackcloudprovider.New(p.client, p.cluster, remote)
 }
 
 // newCiliumProvisioner wraps up Cilium provisioner configuration.
-func (p *Provisioner) newCiliumProvisioner() *cilium.Provisioner {
-	return cilium.New(p.client, p.cluster, p.argoCDClusterName())
+func (p *Provisioner) newCiliumProvisioner(remote string) *cilium.Provisioner {
+	return cilium.New(p.client, p.cluster, remote)
 }
 
-// argoCDClusterName returns a human readable server name.
-func (p *Provisioner) argoCDClusterName() string {
-	return fmt.Sprintf("kubernetes-%s-%s-%s", p.cluster.Labels[constants.ProjectLabel], p.cluster.Labels[constants.ControlPlaneLabel], p.cluster.Name)
-}
-
-// getKubernetesClusterConfig retrieves the Kubernetes configuration from
-// a cluster API cluster.
-func (p *Provisioner) getKubernetesClusterConfig(ctx context.Context) (*clientcmdapi.Config, error) {
-	vc := vcluster.NewControllerRuntimeClient(p.client)
-
-	vclusterClient, err := vc.Client(ctx, p.cluster.Namespace, false)
-	if err != nil {
-		return nil, fmt.Errorf("%w: failed to get vcluster client", err)
+func (p *Provisioner) getRemoteClusterGenerator() *clusteropenstack.RemoteClusterGenerator {
+	clusterLabels := []string{
+		p.cluster.Labels[constants.ControlPlaneLabel],
+		p.cluster.Labels[constants.ProjectLabel],
 	}
 
-	secret := &corev1.Secret{}
-
-	secretKey := client.ObjectKey{
-		Namespace: p.cluster.Name,
-		Name:      p.cluster.Name + "-kubeconfig",
-	}
-
-	// Retry getting the secret until it exists.
-	getSecret := func() error {
-		return vclusterClient.Get(ctx, secretKey, secret)
-	}
-
-	if err := retry.Forever().DoWithContext(ctx, getSecret); err != nil {
-		return nil, err
-	}
-
-	config, err := clientcmd.NewClientConfigFromBytes(secret.Data["value"])
-	if err != nil {
-		return nil, err
-	}
-
-	rawConfig, err := config.RawConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	return &rawConfig, nil
+	return clusteropenstack.NewRemoteClusterGenerator(p.vclusterClient, p.cluster.Namespace, p.cluster.Name, clusterLabels)
 }
 
 // Provision implements the Provision interface.
 func (p *Provisioner) Provision(ctx context.Context) error {
-	config, err := p.getKubernetesClusterConfig(ctx)
-	if err != nil {
-		return err
-	}
+	rcg := p.getRemoteClusterGenerator()
 
-	server := config.Clusters[config.Contexts[config.CurrentContext].Cluster].Server
-
-	argocd, err := argocdclient.NewInCluster(ctx, p.client, "argocd")
-	if err != nil {
-		return err
-	}
-
-	// Retry adding the cluster until ArgoCD deems it's ready, it'll 500 until that
-	// condition is met.
-	upsertCluster := func() error {
-		if err := argocdcluster.Upsert(ctx, argocd, p.argoCDClusterName(), server, config); err != nil {
-			return err
-		}
-
-		return nil
-	}
-
-	if err := retry.Forever().DoWithContext(ctx, upsertCluster); err != nil {
+	if err := remotecluster.New(p.client, rcg).Provision(ctx); err != nil {
 		return err
 	}
 
 	group := concurrent.Provisioner{
 		Group: "cluster add-ons",
 		Provisioners: []provisioners.Provisioner{
-			p.newCiliumProvisioner(),
-			p.newOpenstackCloudProviderProvisioner(),
+			p.newCiliumProvisioner(rcg.Name()),
+			p.newOpenstackCloudProviderProvisioner(rcg.Name()),
 		},
 	}
 
@@ -159,11 +101,13 @@ func (p *Provisioner) Provision(ctx context.Context) error {
 
 // Deprovision implements the Provision interface.
 func (p *Provisioner) Deprovision(ctx context.Context) error {
+	rcg := p.getRemoteClusterGenerator()
+
 	group := concurrent.Provisioner{
 		Group: "cluster add-ons",
 		Provisioners: []provisioners.Provisioner{
-			p.newCiliumProvisioner(),
-			p.newOpenstackCloudProviderProvisioner(),
+			p.newCiliumProvisioner(rcg.Name()),
+			p.newOpenstackCloudProviderProvisioner(rcg.Name()),
 		},
 	}
 
@@ -171,14 +115,7 @@ func (p *Provisioner) Deprovision(ctx context.Context) error {
 		return err
 	}
 
-	// TODO: Can I delete the cluster and then have all the dependent applications
-	// get magically cleaned up?
-	argocd, err := argocdclient.NewInCluster(ctx, p.client, "argocd")
-	if err != nil {
-		return err
-	}
-
-	if err := argocdcluster.Delete(ctx, argocd, p.argoCDClusterName()); err != nil {
+	if err := remotecluster.New(p.client, rcg).Deprovision(ctx); err != nil {
 		return err
 	}
 
