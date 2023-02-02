45b4fbc0819fd47a8bb4fb260dbe7751d5cf53e4
Simon Murray <spjmurray@yahoo.co.uk>
2023-02-02 15:19:36 +0000

    OpenAPI Request/Result Validation (#113)
    First up add some more static validation to ensure our policies are
    being enforced with API design/security.  Add in validation so we can
    reject requestes outright that fail our validation, and warn when we
    cock up results.  The OAPI validation also delegates authn/authz to a
    callback, so we may as well use that rather than maintaining code and
    using a noop.

--- hack/validate_openapi/main.go
+++ hack/validate_openapi/main.go
@@ -19,20 +19,103 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/eschercloudai/unikorn/pkg/server/generated"
 )
 
+//nolint:gochecknoglobals
+var failed bool
+
+func report(v ...interface{}) {
+	fmt.Println(v...)
+
+	failed = true
+}
+
+//nolint:gocognit,cyclop
 func main() {
 	spec, err := generated.GetSwagger()
 	if err != nil {
-		fmt.Println("failed to load spec", err)
-		os.Exit(1)
+		report("failed to load spec", err)
 	}
 
 	if err := spec.Validate(context.Background()); err != nil {
-		fmt.Println("failed to validate spec", err)
+		report("failed to validate spec", err)
+	}
+
+	for pathName, path := range spec.Paths {
+		for method, operation := range path.Operations() {
+			// Everything needs security defining.
+			if operation.Security == nil {
+				report("no security requirements set for ", method, pathName)
+				os.Exit(1)
+			}
+
+			// If you have multiple, then the errors become ambiguous to handle.
+			if len(*operation.Security) != 1 {
+				report("security requirement for", method, pathName, "require one security requirement")
+				os.Exit(1)
+			}
+
+			if method == http.MethodGet {
+				// GET calls will have a response.
+				if len(operation.Responses) == 0 {
+					report("no response set for", method, pathName)
+				}
+			}
+
+			// Where there are responses, they must have a schema.
+			for code, responseRef := range operation.Responses {
+				response := responseRef.Value
+				if response == nil {
+					response = spec.Components.Responses[responseRef.Ref].Value
+				}
+
+				if response.Content == nil {
+					report("no content type set for", code, method, pathName, "response")
+				}
+
+				for mimeType, mediaType := range response.Content {
+					if mediaType.Schema == nil {
+						report("no schema set for", mimeType, code, method, pathName, "response")
+					}
+				}
+			}
+
+			//nolint:nestif
+			if method == http.MethodPost || method == http.MethodPut {
+				// You have to explicitly opt out from following the rules.
+				_, noBodyAllowed := operation.Extensions["x-no-body"]
+
+				// POST/PUT calls will have something to validate.
+				if operation.RequestBody == nil {
+					if noBodyAllowed {
+						continue
+					}
+
+					report("no request body set for", method, pathName)
+
+					continue
+				}
+
+				body := operation.RequestBody.Value
+				if body == nil {
+					body = spec.Components.RequestBodies[operation.RequestBody.Ref].Value
+				}
+
+				// Request bodies will have a schema.
+				for mimeType, mediaType := range body.Content {
+					if mediaType.Schema == nil {
+						report("no schema set for", mimeType, method, pathName)
+					}
+				}
+			}
+		}
+	}
+
+	if failed {
 		os.Exit(1)
 	}
 }
--- pkg/server/middleware/authorization.go
+++ pkg/server/middleware/authorization.go
@@ -21,17 +21,18 @@ import (
 	"strings"
 
 	"github.com/getkin/kin-openapi/openapi3"
-	"github.com/getkin/kin-openapi/routers/gorillamux"
 
 	"github.com/eschercloudai/unikorn/pkg/server/authorization"
-	"github.com/eschercloudai/unikorn/pkg/server/context"
 	"github.com/eschercloudai/unikorn/pkg/server/errors"
-	"github.com/eschercloudai/unikorn/pkg/server/generated"
 )
 
 // authorizationContext is passed through the middleware to propagate
 // information back to the top level handler.
 type authorizationContext struct {
+	// err allows us to return a verbose error, unwrapped by whatever
+	// the openapi validaiton is doing.
+	err error
+
 	// subject is the token subject.
 	subject string
 
@@ -43,13 +44,7 @@ type authorizationContext struct {
 type Authorizer struct {
 	// issuer allows creation and validation of JWT bearer tokens.
 	issuer *authorization.JWTIssuer
-
-	// next defines the next HTTP handler in the chain.
-	next http.Handler
 }
-
-// Ensure this implements the required interfaces.
-var _ http.Handler = &Authorizer{}
 
 // NewAuthorizer returns a new authorizer with required parameters.
 func NewAuthorizer(issuer *authorization.JWTIssuer) *Authorizer {
@@ -94,7 +89,7 @@ func (a *Authorizer) authorizeOAuth2(ctx *authorizationContext, r *http.Request,
 	// Check the token is authorized to do what the schema says.
 	for _, scope := range scopes {
 		if !claims.Scope.Includes(authorization.Scope(scope)) {
-			return errors.OAuth2AccessDenied("token missing required scope").WithValues("scope", scope)
+			return errors.OAuth2InvalidScope("token missing required scope").WithValues("scope", scope)
 		}
 	}
 
@@ -116,110 +111,4 @@ func (a *Authorizer) authorizeScheme(ctx *authorizationContext, r *http.Request,
 	}
 
 	return errors.OAuth2InvalidRequest("authorization scheme unsupported").WithValues("scheme", scheme.Type)
-}
-
-// authorizeSchemes requires all schemes to be fulfilled.
-func (a *Authorizer) authorizeSchemes(ctx *authorizationContext, r *http.Request, spec *openapi3.T, requirement openapi3.SecurityRequirement) error {
-	if len(requirement) == 0 {
-		return errors.OAuth2ServerError("no security schemes specified for operation")
-	}
-
-	for schemeName, scopes := range requirement {
-		scheme, ok := spec.Components.SecuritySchemes[schemeName]
-		if !ok {
-			return errors.OAuth2ServerError("security scheme missing from schema")
-		}
-
-		if scheme.Value == nil {
-			return errors.OAuth2ServerError("security scheme reference not supported")
-		}
-
-		if err := a.authorizeScheme(ctx, r, scheme.Value, scopes); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// authorizeReqirements requires at least one set of requirements to be fulfilled.
-func (a *Authorizer) authorizeReqirements(ctx *authorizationContext, r *http.Request, spec *openapi3.T, requirements openapi3.SecurityRequirements) error {
-	// Why you ask?  Well if we allowed multiple, that means any must be authoried
-	// to succeed, but then that raises the problem of how do you report multiple
-	// errors to the client if they all fail in a meaningful way?
-	if len(requirements) != 1 {
-		return errors.OAuth2ServerError("single security requirements required")
-	}
-
-	if err := a.authorizeSchemes(ctx, r, spec, requirements[0]); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// authorize performs any authorization of the request before allowing access to
-// the resources via the handler.
-func (a *Authorizer) authorize(ctx *authorizationContext, r *http.Request) error {
-	spec, err := generated.GetSwagger()
-	if err != nil {
-		return errors.OAuth2ServerError("unable to decode openapi schema").WithError(err)
-	}
-
-	router, err := gorillamux.NewRouter(spec)
-	if err != nil {
-		return errors.OAuth2ServerError("unable to create router").WithError(err)
-	}
-
-	// Authentication is dependant on route, so we insert this middleware
-	// after the route, and let the router handle 404/405 errors, so this
-	// logic should always work.
-	route, _, err := router.FindRoute(r)
-	if err != nil {
-		return errors.OAuth2ServerError("unable to find route").WithError(err)
-	}
-
-	// You must have specified a security requirement in the schema.
-	if route.Operation == nil {
-		return errors.OAuth2ServerError("unable to find route operation")
-	}
-
-	if route.Operation.Security == nil {
-		return errors.OAuth2ServerError("unable to find route operation security requirements")
-	}
-
-	// Check the necessary security is in place.
-	if err := a.authorizeReqirements(ctx, r, spec, *route.Operation.Security); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// ServeHTTP implements the http.Handler interface.
-func (a *Authorizer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctx := &authorizationContext{}
-
-	if err := a.authorize(ctx, r); err != nil {
-		errors.HandleError(w, r, err)
-
-		return
-	}
-
-	c := r.Context()
-
-	// Add any contextual information to bubble up to the handler.
-	c = context.NewContextWithSubject(c, ctx.subject)
-	c = context.NewContextWithToken(c, ctx.token)
-
-	r = r.WithContext(c)
-
-	a.next.ServeHTTP(w, r)
-}
-
-// Middleware performs any authorization handling middleware.
-func (a *Authorizer) Middleware(next http.Handler) http.Handler {
-	a.next = next
-
-	return a
 }
