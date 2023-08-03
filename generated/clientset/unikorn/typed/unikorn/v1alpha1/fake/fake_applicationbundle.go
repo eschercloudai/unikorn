78be7d0318dc2805a8c154c1518d121ea0402a46
Simon Murray <spjmurray@yahoo.co.uk>
2023-08-03 10:53:12 +0100

    Use CEL to Lock Down Autoscaling Ranges
    Only allow the resource if max is larger than min, if they are equal it
    doesn't make sense, don't use autoscaling!

--- generated/clientset/unikorn/typed/unikorn/v1alpha1/fake/fake_applicationbundle.go
+++ generated/clientset/unikorn/typed/unikorn/v1alpha1/fake/fake_applicationbundle.go
@@ -24,7 +24,6 @@ import (
 	v1alpha1 "github.com/eschercloudai/unikorn/pkg/apis/unikorn/v1alpha1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	testing "k8s.io/client-go/testing"
@@ -35,9 +34,9 @@ type FakeApplicationBundles struct {
 	Fake *FakeUnikornV1alpha1
 }
 
-var applicationbundlesResource = schema.GroupVersionResource{Group: "unikorn.eschercloud.ai", Version: "v1alpha1", Resource: "applicationbundles"}
+var applicationbundlesResource = v1alpha1.SchemeGroupVersion.WithResource("applicationbundles")
 
-var applicationbundlesKind = schema.GroupVersionKind{Group: "unikorn.eschercloud.ai", Version: "v1alpha1", Kind: "ApplicationBundle"}
+var applicationbundlesKind = v1alpha1.SchemeGroupVersion.WithKind("ApplicationBundle")
 
 // Get takes name of the applicationBundle, and returns the corresponding applicationBundle object, and an error if there is any.
 func (c *FakeApplicationBundles) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.ApplicationBundle, err error) {
