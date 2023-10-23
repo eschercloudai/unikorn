e4dfa0a419136097b3241831e4a8ff2839d0c843
Simon Murray <spjmurray@yahoo.co.uk>
2023-10-23 12:10:53 +0100

    Architecture Documentation
    Add in some architecture documentation for developers to read, rather
    than writing code blindly and having to ask questions.  Also removes
    some dead code discovered during code review.  Finally enables codecov
    during CI to ensure stuff is tested adequately.

--- pkg/managers/cluster/manager.go
+++ pkg/managers/cluster/manager.go
@@ -17,80 +17,19 @@ limitations under the License.
 package cluster
 
 import (
-	"context"
-
 	unikornv1 "github.com/eschercloudai/unikorn/pkg/apis/unikorn/v1alpha1"
 	"github.com/eschercloudai/unikorn/pkg/managers/common"
 	"github.com/eschercloudai/unikorn/pkg/managers/options"
 	"github.com/eschercloudai/unikorn/pkg/provisioners/managers/cluster"
 
-	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/util/workqueue"
-
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
-
-// eventHandlerOwnerFromLabel extracts a parent resource from a resource label and
-// enqueues a reconconcile request for it.  Useful when subordinate objects are
-// gathered via a label selector.
-type eventHandlerOwnerFromLabel struct {
-	// label is the label to look for.
-	label string
-}
-
-// Ensure the handler.EventHandler interface is implemented.
-var _ handler.EventHandler = &eventHandlerOwnerFromLabel{}
-
-// Create is called in response to an create event - e.g. Pod Creation.
-func (e *eventHandlerOwnerFromLabel) Create(ctx context.Context, evt event.CreateEvent, q workqueue.RateLimitingInterface) {
-	e.enqueue(evt.Object, q)
-}
-
-// Update is called in response to an update event -  e.g. Pod Updated.
-func (e *eventHandlerOwnerFromLabel) Update(ctx context.Context, evt event.UpdateEvent, q workqueue.RateLimitingInterface) {
-	e.enqueue(evt.ObjectNew, q)
-	e.enqueue(evt.ObjectOld, q)
-}
-
-// Delete is called in response to a delete event - e.g. Pod Deleted.
-func (e *eventHandlerOwnerFromLabel) Delete(ctx context.Context, evt event.DeleteEvent, q workqueue.RateLimitingInterface) {
-	e.enqueue(evt.Object, q)
-}
-
-// Generic is called in response to an event of an unknown type or a synthetic event triggered as a cron or
-// external trigger request - e.g. reconcile Autoscaling, or a Webhook.
-func (e *eventHandlerOwnerFromLabel) Generic(ctx context.Context, evt event.GenericEvent, q workqueue.RateLimitingInterface) {
-	e.enqueue(evt.Object, q)
-}
-
-// enqueue adds a reconcile request to the queue if the requested label exists and uses that
-// to map to the resource name.
-func (e *eventHandlerOwnerFromLabel) enqueue(object client.Object, q workqueue.RateLimitingInterface) {
-	if object == nil {
-		return
-	}
-
-	name, ok := object.GetLabels()[e.label]
-	if !ok {
-		return
-	}
-
-	request := reconcile.Request{
-		NamespacedName: types.NamespacedName{
-			Name:      name,
-			Namespace: object.GetNamespace(),
-		},
-	}
-
-	q.Add(request)
-}
 
 // Factory provides methods that can build a type specific controller.
 type Factory struct{}
