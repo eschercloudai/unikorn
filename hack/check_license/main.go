92b6908bcecec6fff538ed00020b45d42d773232
Simon Murray <spjmurray@yahoo.co.uk>
2024-01-02 09:42:55 +0000

    Update Copyright

--- hack/check_license/main.go
+++ hack/check_license/main.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022-2023 EscherCloud.
+Copyright 2022-2024 EscherCloud.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ const (
 	// TODO: may want to make this fuzzy (e.g. the date) and make it
 	// a regex match.
 	goApache2LicenseHeader = `/*
-Copyright 2022-2023 EscherCloud.
+Copyright 2022-2024 EscherCloud.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
